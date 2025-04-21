06f07de0d0eabd8c41587bafa838872d1caa8eb7
zayn <zayn442@hotmail.com>
2025-04-21 18:49:20 +0100

    UploadPage4

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,14 +1,12 @@
 import Link from 'next/link';
-import styles from '../styles/Toolbar.module.css';
+import styles from '../styles/UploadPage.module.css';
 
-const Toolbar = () => {
-  return (
-    <nav className={styles.toolbar}>
-      <Link href="/upload" className={styles.tab}>Import</Link>
-      <Link href="/annotate" className={styles.tab}>Annotate</Link>
-      <Link href="/export" className={styles.tab}>Export</Link>
-    </nav>
-  );
-};
+const Toolbar = () => (
+  <div className={styles.toolbar}>
+    <Link href="/upload" className={`${styles.tab} ${styles.active}`}>Import</Link>
+    <Link href="/annotate" className={styles.tab}>Annotate</Link>
+    <Link href="/export" className={styles.tab}>Export</Link>
+  </div>
+);
 
 export default Toolbar;

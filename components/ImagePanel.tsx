3701ab3643dd85fdd6e95e872516dde477e9435c
zayn <zayn442@hotmail.com>
2025-04-29 19:22:04 +0100

    css recode3

--- components/ImagePanel.tsx
+++ components/ImagePanel.tsx
@@ -18,43 +18,39 @@ export default function ImagePanel({ images, currentIndex, onPrev, onNext }: Ima
 
   return (
     <div className={styles.imageNavigator}>
-      
+      {/* LEFT SIDE */}
       <div className={styles.sidePanel}>
-        {hasPrev ? (
-          <>
-            <button className={styles.arrowButton} onClick={onPrev}>&lt;</button>
-            <div className={styles.thumbnail}>
-              <img src={images[currentIndex - 1]} alt="Previous" />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={styles.arrowSpacer} />
-            <div className={styles.thumbnail} />
-          </>
-        )}
+        <button
+          className={styles.arrowButton}
+          onClick={onPrev}
+          disabled={!hasPrev}
+          style={{ visibility: hasPrev ? 'visible' : 'hidden' }}
+        >
+          &lt;
+        </button>
+        <div className={styles.thumbnail}>
+          {hasPrev && <img src={images[currentIndex - 1]} alt="Previous" />}
+        </div>
       </div>
 
-      
+      {/* MAIN IMAGE */}
       <div className={styles.mainImage}>
         {imageUrl && <CanvasAnnotator imageUrl={imageUrl} width={300} height={400} />}
       </div>
 
-      
+      {/* RIGHT SIDE */}
       <div className={styles.sidePanel}>
-        {hasNext ? (
-          <>
-            <div className={styles.thumbnail}>
-              <img src={images[currentIndex + 1]} alt="Next" />
-            </div>
-            <button className={styles.arrowButton} onClick={onNext}>&gt;</button>
-          </>
-        ) : (
-          <>
-            <div className={styles.thumbnail} />
-            <div className={styles.arrowSpacer} />
-          </>
-        )}
+        <div className={styles.thumbnail}>
+          {hasNext && <img src={images[currentIndex + 1]} alt="Next" />}
+        </div>
+        <button
+          className={styles.arrowButton}
+          onClick={onNext}
+          disabled={!hasNext}
+          style={{ visibility: hasNext ? 'visible' : 'hidden' }}
+        >
+          &gt;
+        </button>
       </div>
     </div>
   );
